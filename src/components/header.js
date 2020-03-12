dd889c8b1e9ed3b9b719b51f3b1b7ffca2db226b
straycatse <developer@straycat.nu>
2020-03-13 00:38:22 +0100

    burgerworking

--- src/components/header.js
+++ src/components/header.js
@@ -7,81 +7,33 @@ import { slide as Menu } from "react-burger-menu"
 
 import "./styles/header.css"
 
-if (typeof window !== "undefined") {
-  const isMobile = window.innerWidth < 480
-}
-console.log(isMobile)
-var styles = {
-  bmBurgerButton: {
-    position: "fixed",
-    width: "36px",
-    height: "30px",
-    right: "36px",
-    top: "36px",
-  },
-  bmBurgerBars: {
-    background: "#373a47",
-  },
-  bmBurgerBarsHover: {
-    background: "#a90000",
-  },
-  bmCrossButton: {
-    height: "24px",
-    width: "24px",
-  },
-  bmCross: {
-    background: "#bdc3c7",
-  },
-  bmMenuWrap: {
-    position: "fixed",
-    height: "100%",
-  },
-  bmMenu: {
-    background: "#373a47",
-    padding: "2.5em 1.5em 0",
-    fontSize: "1.15em",
-  },
-  bmMorphShape: {
-    fill: "#373a47",
-  },
-  bmItemList: {
-    color: "#b8b7ad",
-    padding: "0.8em",
-  },
-  bmItem: {
-    display: "inline-block",
-  },
-  bmOverlay: {
-    background: "rgba(0, 0, 0, 0.3)",
-  },
-}
-
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `white`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+  <React.Fragment>
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 1920,
-        padding: `1.45rem 1.3rem`,
-        minHeight: "10vh",
+        background: `white`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <Link
-        to="/"
+      <div
         style={{
-          color: `red`,
-          textDecoration: `none`,
+          margin: `0 auto`,
+          maxWidth: 1920,
+          padding: `1.45rem 1.3rem`,
+          minHeight: "10vh",
         }}
       >
-        <img style={{ maxWidth: 200 }} src={logo} alt={siteTitle}></img>
-      </Link>
-      {!isMobile && (
-        <nav style={{ float: "right", margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `red`,
+            textDecoration: `none`,
+          }}
+        >
+          <img style={{ maxWidth: 200 }} src={logo} alt={siteTitle}></img>
+        </Link>
+
+        <nav className="desktopMenu" style={{ float: "right", margin: 0 }}>
           <a className="navLink" href="https://beta.sekreterare.nu/signin">
             Logga in
           </a>
@@ -90,20 +42,9 @@ const Header = ({ siteTitle }) => (
             text="Skapa konto"
           ></PrimaryButton>
         </nav>
-      )}
-      {isMobile && (
-        <Menu styles={styles} right>
-          <a className="navLink" href="https://beta.sekreterare.nu/signin">
-            Logga in
-          </a>
-          <PrimaryButton
-            link="https://beta.sekreterare.nu/signup"
-            text="Skapa konto"
-          ></PrimaryButton>
-        </Menu>
-      )}
-    </div>
-  </header>
+      </div>
+    </header>
+  </React.Fragment>
 )
 
 Header.propTypes = {
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,8 +11,55 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
+import { slide as Menu } from "react-burger-menu"
+import PrimaryButton from "../components/primarybutton"
 
 import "./styles/layout.css"
+
+var styles = {
+  bmBurgerButton: {
+    position: "fixed",
+    width: "36px",
+    height: "30px",
+    right: "36px",
+    top: "36px",
+  },
+  bmBurgerBars: {
+    background: "#373a47",
+  },
+  bmBurgerBarsHover: {
+    background: "#a90000",
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px",
+  },
+  bmCross: {
+    background: "#bdc3c7",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    height: "100%",
+  },
+  bmMenu: {
+    background: "#373a47",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+  bmMorphShape: {
+    fill: "#373a47",
+  },
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em",
+  },
+  bmItem: {
+    display: "inline-block",
+  },
+  bmOverlay: {
+    background: "rgba(0, 0, 0, 0.3)",
+  },
+}
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -27,9 +74,24 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Menu
+        outerContainerId={"gatsby-focus-wrapper"}
+        pageWrapId={"innerWrap"}
+        styles={styles}
+        className="burgerMenu"
+        right
+      >
+        <a className="navLink" href="https://beta.sekreterare.nu/signin">
+          Logga in
+        </a>
+        <PrimaryButton
+          link="https://beta.sekreterare.nu/signup"
+          text="Skapa konto"
+        ></PrimaryButton>
+      </Menu>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div>
-        <main style={{}}>{children}</main>
+        <main id="innerWrap">{children}</main>
       </div>
       <Footer></Footer>
     </>
